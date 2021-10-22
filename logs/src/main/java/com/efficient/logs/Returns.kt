ef8288c54fd0e4af07f7b37a6b9314bd42e08048
Izyver <mr.izyver@gmail.com>
2021-10-22 21:42:14 +0300

    Added returns file

---
+++ logs/src/main/java/com/efficient/logs/Returns.kt
@@ -0,0 +1,16 @@
+package com.efficient.logs
+
+private var throwingAction: (() -> Unit)? = null
+fun setThrowingAction(action: () -> Unit) {
+    throwingAction = null
+}
+
+fun <T> throwing(throwable: Throwable? = null): T? {
+    val exception = Error(throwable ?: RuntimeException("Something went wrong"))
+    if (BuildConfig.LOG_ON) {
+        throw exception
+    } else {
+        throwingAction?.invoke()
+        return null
+    }
+}
