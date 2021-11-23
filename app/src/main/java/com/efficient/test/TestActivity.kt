d2772870778304d9e064542bd1c1a1f11e72ade7
izyver <mr.izyver@gmail.com>
2021-11-23 11:48:20 +0200

    Updated test app

--- app/src/main/java/com/efficient/test/TestActivity.kt
+++ app/src/main/java/com/efficient/test/TestActivity.kt
@@ -3,10 +3,19 @@ package com.efficient.test
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.efficient.logs.logd
+import com.efficient.logs.logfile
+import java.io.File
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        logd { "Hello" }
+        val file = File(cacheDir, "log.txt")
+        file.deleteOnExit()
+        logfile(file)
+        logd { "Hello0" }
+        logd { "Hello1" }
+        logd { "Hello2" }
+        logd { "Hello3" }
+        println()
     }
 }
